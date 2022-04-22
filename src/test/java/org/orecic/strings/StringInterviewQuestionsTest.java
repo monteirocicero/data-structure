97c03db09e2599182684893ea0a8228f1ee9051c
Cicero Monteiro <cicerolmonteiro@gmail.com>
2022-04-22 15:38:49 -0300

    solved urlify

--- src/test/java/org/orecic/strings/StringInterviewQuestionsTest.java
+++ src/test/java/org/orecic/strings/StringInterviewQuestionsTest.java
@@ -32,13 +32,37 @@ public class StringInterviewQuestionsTest {
     public void testTwoStringIsPermutation() {
         // given
         String strA = "xyz";
-        String strB = "xzy";
 
         List<String> permutationsCases = Arrays.asList("XYZ", "XZY", "ZXY", "ZYX", "YZX", "YXZ");
 
         for (int i = 0; i < permutationsCases.size(); i++) {
             Assert.assertTrue(hasPermutation(strA, permutationsCases.get(i)));
         }
+    }
+
+    @Test
+    public void testURLify() {
+        // given
+        String input = "Mr John Smith   ";
+        int length = 13;
+        String output = "Mr%20John%20Smith";
+
+        // then
+        Assert.assertEquals(output, replaceSpacesFromString(input, length));
+    }
+
+    private String replaceSpacesFromString(String input, int length) {
+        char[] inputChar = input.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            if (new Character(' ').equals(inputChar[i])) {
+                sb.append("%20");
+                continue;
+            }
+            sb.append(inputChar[i]);
+        }
+        return sb.toString();
     }
 
     private boolean hasPermutation(String strA, String strB) {
